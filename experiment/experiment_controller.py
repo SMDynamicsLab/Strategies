ce896e4f8bbabe97ff15d8934e99e939b1f5d1d2
galilei83 <arieldario.silva83@gmail.com>
2022-07-11 15:43:00 -0300

    Python: Función Load_TrialData con función Compute_Asyn embebida.

--- experiment/experiment_controller.py
+++ experiment/experiment_controller.py
@@ -479,7 +479,6 @@ def Load_SubjectMetadata(subject_number, total_blocks):
 		conc_block_conditions_df = conc_block_conditions_df.append(file_to_load_df)	
 	conc_block_conditions_df = conc_block_conditions_df.reset_index()
 	conc_block_conditions_df = conc_block_conditions_df.drop(columns = ['index'])
-	conc_block_conditions_df.to_csv('./Data/auxi' + s_number + '.csv')
 	return conc_block_conditions_df
 
 
@@ -496,7 +495,7 @@ def Load_ExpMetadata():
 		conc_blocks_conditions_df = conc_blocks_conditions_df.append(blocks_conditions_df)
 	conc_blocks_conditions_df = conc_blocks_conditions_df.reset_index()
 	conc_blocks_conditions_df = conc_blocks_conditions_df.drop(columns = ['index'])
-	conc_blocks_conditions_df.to_csv('./Data/auxiliar.csv')
+	conc_blocks_conditions_df.to_csv('./Data/ExpMetaData.csv')
 	return conc_blocks_conditions_df
 
 
@@ -516,27 +515,43 @@ def Load_TrialData(subject_number, block, trial):
 	trial_n = []
 	event = []
 	time = []
-	
+	order = []
+	stim_times = []
+	resp_times = []
+	indexS = 0
+	indexR = 0
 	for i in range(len(content['Data'])):
 		event.append(content['Data'][i][0:1])
 		time.append(int(content['Data'][i][4:][:-1]))
 		subject_n.append(s_number)
 		block_n.append(block)
 		trial_n.append(trial)
-	for i in range(len(content['Asynchrony'])):
+		if (event[-1] == 'S'):
+			order.append(indexS)
+			stim_times.append(time[-1])
+			indexS = indexS + 1
+		if (event[-1] == 'R'):
+			order.append(indexR)
+			resp_times.append(time[-1])
+			indexR = indexR + 1
+			
+	asyn_df = Compute_Asyn(stim_times,resp_times)
+	for i in range(len(asyn_df.index)):
 		event.append('A')
-		time.append(content['Asynchrony'][i])
+		time.append(asyn_df['asyn'][i])
 		subject_n.append(s_number)
 		block_n.append(block)
 		trial_n.append(trial)
-		
+		order.append(asyn_df['assigned_stim'][i])
+	
 	trialData_df = pd.DataFrame()
-	trialData_df = trialData_df.assign(Subject = subject_n, Block = block_n, Trial = trial_n, Event = event, Time = time)
+	trialData_df = trialData_df.assign(Subject = subject_n, Block = block_n, Trial = trial_n, Event = event, Event_Order = order, Time = time)
+	trialData_df.to_csv('./Data/TrialData.csv')
 	return trialData_df
 
 
 #%% Load_TrialsData
-# Function to load trials data. Return a dictionary.
+# Function to load trials data. Return a dataframe.
 def Load_TrialsData():
 	expMetadata_df = Load_ExpMetadata()
 	conc_trialData_df = pd.DataFrame()
@@ -548,6 +563,85 @@ def Load_TrialsData():
 		conc_trialData_df = conc_trialData_df.append(trialData_df)
 	conc_trialData_df = conc_trialData_df.reset_index()
 	conc_trialData_df = conc_trialData_df.drop(columns = ['index'])
-	conc_trialData_df.to_csv('./Data/auxiliar2.csv')
+	conc_trialData_df.to_csv('./Data/TrialsData.csv')
 	return conc_trialData_df
+
+
+#%% compute_asyn
+# Compute asynchronies from stimulus and response time occurrences. Return a dataframe.
+def Compute_Asyn(stim_times,resp_times):
+	if len(resp_times)==0:
+		asyn = []
+		assigned_stim = []
+	else:
+		ISI = np.median(np.diff(stim_times))
+		asyn_max = round(ISI/2)
+		n_stims = len(stim_times)
+		n_resps = len(resp_times)
+		minima_R = np.zeros((n_stims,n_resps),dtype=int)
+		minima_S = np.zeros((n_stims,n_resps),dtype=int)
+		assigned_stim = -np.ones(n_resps,dtype=int)
+		stimuli_flag = np.zeros(n_stims,dtype=int)
+		asyn = np.full(n_resps,np.nan)
+		asyn_max = ISI/2
+	
+		# Find matching S-R pairs
+	
+		# pairwise differences between every response and every stimulus
+		# (dimensions = number of stimuli x number of responses)
+		differences = -np.subtract.outer(stim_times,resp_times).astype(float)	# type float so it can be NaN
+		differences[abs(differences)>=asyn_max] = np.nan # remove differences larger than threshold
+	
+		# for every response, find the closest stimulus (nontrivial if more responses than stimuli)
+		# IM SURE THIS LOOP CAN BE VECTORIZED
+		for resp in range(n_resps):
+			aux = differences[:,resp]
+			# prevent "no non-missing arguments to min; returning Inf" warning
+			#if np.any(np.isnan(aux)): # find at least one non-missing value
+			min_abs = np.nanmin(abs(aux))
+			min_idx = np.where(abs(aux)==min_abs)
+			minima_R[min_idx,resp] = 1
+	
+	
+		# remove multiple responses closest to a single stimulus (row-wise consecutive 1's)
+		# i.e. make no attempt at deciding
+		minima_shift_R = minima_R + np.roll(minima_R,(0,1),(0,1)) + np.roll(minima_R,(0,-1),(0,1))
+		minima_R[np.where(minima_shift_R>=2)] = 0
+	
+		# for every stimulus, find the closest response (nontrivial if more stimuli than responses)
+		# IM SURE THIS LOOP CAN BE VECTORIZED
+		for stim in range(n_stims):
+			aux = differences[stim,:]
+			# prevent "no non-missing arguments to min; returning Inf" warning
+			#if np.any(np.isnan(aux)): # find at least one non-missing value
+			min_abs = np.nanmin(abs(aux))
+			min_idx = np.where(abs(aux)==min_abs)
+			minima_S[stim,min_idx] = 1
+	
+		# remove multiple stimuli closest to a single response (col-wise consecutive 1's)
+		# i.e. make no attempt at deciding
+		minima_shift_S = minima_S + np.roll(minima_S,(1,0),(0,1)) + np.roll(minima_S,(-1,0),(0,1))
+		minima_S[np.where(minima_shift_S>=2)] = 0
+	
+		# matching pairs are represented by intersections (i.e. common 1's)
+		minima_intersect = minima_R*minima_S
+	
+		# save asynchronies: get row and column for every matched pair
+		SR_idxs = np.where(minima_intersect==1)
+		S_idx = SR_idxs[0]
+		R_idx = SR_idxs[1]
+	
+		# keep track of which stimulus was assigned to each response (NaN if not assigned)
+		assigned_stim[R_idx] = S_idx
+		# keep track of assigned stimuli (-1 if not assigned)
+		stimuli_flag[S_idx] = np.ones(len(S_idx))
+	
+		# save asynchrony (NA means that response was not assigned)
+		if (S_idx.size != 0 and R_idx.size != 0):
+			lin_ind = np.ravel_multi_index((S_idx,R_idx),differences.shape)
+			asyn[R_idx] = differences.ravel()[lin_ind]
+	
+	# Output
+	output = pd.DataFrame({'asyn':asyn,'assigned_stim':assigned_stim})
+	return output
 
